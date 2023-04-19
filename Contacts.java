0aa4c07ce3256a1945895640628fe5dc51456674
Tanaya <desaitanaya17@gmail.com>
2023-04-19 16:05:48 +0530

    [Tanaya] Add : Java Program to create Contacts in Address Book.

---
+++ Contacts.java
@@ -0,0 +1,81 @@
+public class Contacts{
+	String first_name, last_name, address, city, state, email;
+	int zipcode;
+	long phone_number;
+	
+	public Contacts(String first_name, String last_name, String address, String city, String state, String email, int zipcode, int phone_number) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.email = email;
+		this.phone_number = phone_number;
+		this.zipcode = zipcode;
+	}
+
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	public long getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(long phone_number) {
+		this.phone_number = phone_number;
+	}
+	}
